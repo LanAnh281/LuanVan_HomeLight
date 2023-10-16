6f1a6a70d79711299a58abba4fc5602fba918f7a
lananh <anh626801@gmail.com>
2023-10-16 16:08:46 +0700

    route mail

--- Backend/app/route/mail.route.js
+++ Backend/app/route/mail.route.js
@@ -3,12 +3,10 @@ const router = express.Router();
 const mail = require("../controller/mail.controller");
 const authorization = require("../middeware/authorization.middeware");
 
-router
-  .route("/")
-  .post(
-    authorization.authentication,
-    authorization.authorization("soạn thư"),
-    mail.sendMail
-  );
+router.route("/").post(
+  // authorization.authentication,
+  // authorization.authorization("soạn thư"),
+  mail.sendMail
+);
 
 module.exports = router;
